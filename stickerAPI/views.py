c52d80920b8031d41cfe59d9f5fd127a014ff4c7
Otacilio Lacerda <otaciliolacerda@gmail.com>
2015-05-21 17:15:09 -0300

    View module support logs

--- stickerAPI/views.py
+++ stickerAPI/views.py
@@ -2,13 +2,17 @@ from django.http import JsonResponse
 from models import *
 from django.core.exceptions import ObjectDoesNotExist
 from seed import *
+import logging
+
+
+log = logging.getLogger(__name__)
 
 
 # Create your views here.
 def needed_stickers(request, user_id):
     #seed()
     stickers = request.REQUEST.get('stickers', None)
-    print 'STICKERS_POST: ' + str(stickers)
+    log.debug('STICKERS_POST: ' + str(stickers))
     if stickers:
         sticker_list = stickers.split(',')
         for i in sticker_list:
@@ -23,5 +27,5 @@ def needed_stickers(request, user_id):
                 return JsonResponse({})
     else:
         needed = NeededStickers.objects.filter(user__id=user_id)
-        print "NEEDED: " + str(needed)
+        log.debug("NEEDED: " + str(needed))
         return JsonResponse([i.dict() for i in needed], safe=False)
